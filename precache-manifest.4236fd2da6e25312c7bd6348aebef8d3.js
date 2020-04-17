a123f9bebe3ffe8e8e2cf7792c64988ef29f734f
Jacob Graczyk <jacobgraczyk@Jacobs-MacBook-Pro.local>
2020-04-17 13:33:08 -0400

    Updates

---
+++ precache-manifest.4236fd2da6e25312c7bd6348aebef8d3.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3c7eb39fc10d79ccbf82090162df1ac7",
+    "url": "/jango/index.html"
+  },
+  {
+    "revision": "3d2ea3990391bab2dc02",
+    "url": "/jango/static/css/main.53851349.chunk.css"
+  },
+  {
+    "revision": "21cb0a5ec2b51c91f3b2",
+    "url": "/jango/static/js/2.0a5d8fa6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/jango/static/js/2.0a5d8fa6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3d2ea3990391bab2dc02",
+    "url": "/jango/static/js/main.e551b6da.chunk.js"
+  },
+  {
+    "revision": "c7c37f4a305cc2cc5077",
+    "url": "/jango/static/js/runtime-main.867bfb72.js"
+  },
+  {
+    "revision": "bc5b07b7b7786d7d1ace8622772edb89",
+    "url": "/jango/static/media/altLogo.bc5b07b7.svg"
+  },
+  {
+    "revision": "ac204e03ad491bbbbffcd117c579b1e9",
+    "url": "/jango/static/media/banner.ac204e03.jpg"
+  },
+  {
+    "revision": "3ea729cdd275f36315ca7b85d0638ff5",
+    "url": "/jango/static/media/logo.3ea729cd.png"
+  },
+  {
+    "revision": "a3c82c6fea12b17093f3d4ee992198c6",
+    "url": "/jango/static/media/productBison.a3c82c6f.jpg"
+  },
+  {
+    "revision": "4919e9d15309419ab7dc958be3be0331",
+    "url": "/jango/static/media/productCaramel.4919e9d1.jpg"
+  },
+  {
+    "revision": "c4ecf6151f66fd699e4d19694de25d7d",
+    "url": "/jango/static/media/productCoconut.c4ecf615.jpg"
+  },
+  {
+    "revision": "da612b7f1e8b5db67327952519b98279",
+    "url": "/jango/static/media/productPeanut.da612b7f.jpg"
+  },
+  {
+    "revision": "54e0a680832a21757be2c2b775c0e477",
+    "url": "/jango/static/media/productSmore.54e0a680.jpg"
+  },
+  {
+    "revision": "a2e13a667377f60ca5aae6f5d58ac7e2",
+    "url": "/jango/static/media/productSponge.a2e13a66.jpg"
+  },
+  {
+    "revision": "5a228b1c13b71e248ce24cd2ab378f2f",
+    "url": "/jango/static/media/texture.5a228b1c.jpg"
+  }
+]);
